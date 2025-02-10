debf76e4ffcf1a2837513d7d3d5a5db879de783a
Ganesan <parvathi.ganesan@accenture.com>
2025-02-10 11:33:37 +0530

    Pushing to repo

---
+++ Demo.java
@@ -0,0 +1,14 @@
+// This is a simple Java program.
+// FileName : "Demo_java.java".
+
+class DemoJava {
+    // Your program begins with a call to main().
+    // Prints message to the terminal window.
+    public static void main(String[] args)
+    {
+        System.out.println("############################");
+        System.out.println("This is pushed to Github");
+        System.out.println("############################");
+    }
+
+}
