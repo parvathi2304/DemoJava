ef43fcb45d8eb577a1ed5332a29fee58191fbbb9
parvathi2304 <shyinthavi@gmail.com>
2025-02-10 10:23:59 +0530

    Update mat.java

--- mat.java
+++ mat.java
@@ -6,6 +6,8 @@ class DemoJava {
     // Prints "Hello, World" to the terminal window.
     public static void main(String[] args)
     {
+         System.out.println("#######################");
         System.out.println("This is Demo project");
+        System.out.println("#######################");
     }
 }
